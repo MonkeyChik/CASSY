49c3c8180c0f44102e88525a51f01981c216445d
Margaret Miriam <134642232+MonkeyChik@users.noreply.github.com>
2023-06-01 17:32:02 -0500

    Did GPT suggested edits

--- api/api-chat.js
+++ api/api-chat.js
@@ -2,14 +2,15 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-  const { message } = req.body;
-
   try {
+    const { message } = await req.json();
+
     const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer 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', // Replace with your actual OpenAI API key
+        'Authorization': 'Bearer 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', 
+        // *****API KEY*****
       },
       body: JSON.stringify({
         prompt: message,
@@ -20,6 +21,12 @@ module.exports = async (req, res) => {
     const data = await response.json();
     const botResponse = data.choices[0].text;
 
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // Send the response
     res.status(200).json({ message: botResponse });
   } catch (error) {
     console.error('Error:', error);
